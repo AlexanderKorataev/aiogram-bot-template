6268581d9ca2048b4afce0f9e4b34b343890fe29
Крупий Владимир <getcher123@gmail.com>
2023-02-18 00:46:01 +0300

    first commit

--- home_parser/parser.py
+++ home_parser/parser.py
@@ -1,3 +1,8 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+
+
 class MyHomeParser:
     _headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                               'Chrome/104.0.5112.124 YaBrowser/22.9.4.863 Yowser/2.5 Safari/537.36'}
